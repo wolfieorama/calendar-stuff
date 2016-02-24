5e769d2755fe5e6326c0a9cce91873ace4467a35
wolfieorama <wachirawamunyi@gmail.com>
2016-02-24 11:39:09 +0300

    mounted engine in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'events#index'
 
+   mount Blazer::Engine, at: "blazer"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
